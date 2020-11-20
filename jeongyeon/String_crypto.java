44622454f98c3281ea26b849d6fbd31f3511a7f5
politics-in-the-pond <74289147+politics-in-the-pond@users.noreply.github.com>
2020-11-20 12:12:21 +0900

    Add files via upload

--- jeongyeon/String_crypto.java
+++ jeongyeon/String_crypto.java
@@ -7,22 +7,26 @@ import javax.crypto.*;
 
 public class String_crypto {
 	
-	final static int N = 624;	//MT19937_32 initialization
-	final static int M = 397;
-	final static long MATRIX_A = 0x9908b0dfL;
-	final static long UMASK = 0x80000000L;
-	final static long LMASK = 0x7fffffffL;
-	static long mt[] = new long[N];
-	static int mti=N+1;
-	
-	private static byte[] encrypt(String s) {
+	final int N = 624;	//MT19937_32 initialization
+	final int M = 397;
+	final long MATRIX_A = 0x9908b0dfL;
+	final long UMASK = 0x80000000L;
+	final long LMASK = 0x7fffffffL;
+
+	private byte[] encrypt(String s) {
 		
 		byte[] buffer;
-		byte[] check_sum;
+		byte[] check_sum = new byte[20];
 		
 		buffer = s.getBytes();
 		check_sum = checksum(buffer);
 		
+		StringBuffer hexString = new StringBuffer();
+        for (int i = 0; i < check_sum.length; i++)
+            hexString.append(Integer.toHexString(0xFF & check_sum[i]));
+        
+        String s3 = hexString.toString();
+				
 		byte seed1[] = new byte[4];
 		byte seed2[] = new byte[4];
 		byte seed3[] = new byte[4];
@@ -45,20 +49,32 @@ public class String_crypto {
 		tempkey = MT19937(seed4);
 		System.arraycopy(tempkey, 0, key, 12, 4);
 		
+		StringBuffer hexString2 = new StringBuffer();
+        for (int i = 0; i < key.length; i++)
+            hexString2.append(Integer.toHexString(0xFF & key[i]));
+        
+        String s1 = hexString2.toString();
+		
+		System.out.println("문자열 : " + s + "		/해시 : " + s3 + "		/AES키 : " + s1);
+		
 		//AES128 암호화 실행 key = key[]
 		
 		return (byte[]) null;
 	}
 	
-	private static String decrypt(byte[] s) {
-		byte[] check_sum = null;
+	
+	
+	private String decrypt(byte[] b) {
+		byte[] check_sum = new byte[20];
+		byte[] s = null;
 		
 		byte seed1[] = new byte[4];
 		byte seed2[] = new byte[4];
 		byte seed3[] = new byte[4];
 		byte seed4[] = new byte[4];
 		
-		System.arraycopy(s, s.length-20, check_sum, 0, 20);
+		System.arraycopy(b, b.length-20, check_sum, 0, 20);
+		System.arraycopy(b, 0, s, 0, b.length-20);
 		
 		System.arraycopy(check_sum, 0, seed1, 0, 4);
 		System.arraycopy(check_sum, 4, seed2, 0, 4);
@@ -82,7 +98,11 @@ public class String_crypto {
 		return (String) null;
 	}
 	
-	private static byte[] MT19937(byte[] seed) { //메르센 트위스터 구현부
+	private byte[] MT19937(byte[] seed) { //메르센 트위스터 구현부. unsigned형이 없어 원본의 MT19937과는 결과값이 다를걸로 예상.
+		
+		long mt[] = new long[N];
+		int mti=N+1;
+		
 		int s = btoi(seed);
 		mt[0] = s & 0xFFFFFFFF;
 		for(mti=1;mti<N;mti++) {
@@ -127,47 +147,31 @@ public class String_crypto {
 		return (byte[]) result;
 	}
 	
-	public static long genrand_int32() {
-		
-		return 0;
-	}
-	
-	@SuppressWarnings("finally")
-	private static byte[] checksum(byte[] message_bin) {
+	private byte[] checksum(byte[] message_bin){
 		try {
-            MessageDigest md = MessageDigest.getInstance("SHA-1");
+			MessageDigest md = MessageDigest.getInstance("SHA-1");
             md.update(message_bin);
-            byte byteData[] = md.digest();
+            byte[] byteData = new byte[20];
+            byteData = md.digest();
             return byteData;
-        } catch(NoSuchAlgorithmException e){
-            e.printStackTrace(); 
-        } finally {
-        	byte error[] = new byte[2];
-        	return error;
-        }
+            
+		} catch(NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		
+		return message_bin;
 	}
 	
-	public static int btoi(byte[] arr){
+	private int btoi(byte[] arr){
 		return (arr[0] & 0xff)<<24 | (arr[1] & 0xff)<<16 | (arr[2] & 0xff)<<8 | (arr[3] & 0xff); 
 	}
 	
-	public static void main(String[] args) {
-		byte[] a = new byte[4];
-		a[0] = 1;
-		a[1] = 2;
-		a[2] = 3;
-		a[3] = 4;
-		System.out.println(MT19937(a));
-		a[0] = 4;
-		a[1] = 32;
-		a[2] = 1;
-		a[3] = 4;
-		System.out.println(MT19937(a));
-		a[0] = 8;
-		a[1] = 4;
-		a[2] = 0;
-		a[3] = 7;
-		System.out.println(MT19937(a));
+	public byte[] do_encrypt(String s){
+		return this.encrypt(s);
+	}
+	
+	public String do_decrypt(byte[] b){
+		return this.decrypt(b);
 	}
 
 }
