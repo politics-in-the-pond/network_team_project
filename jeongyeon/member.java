b983d7d103448804b1b96310a488d1de894eb706
politics-in-the-pond <74289147+politics-in-the-pond@users.noreply.github.com>
2020-12-14 15:40:53 +0900

    Update member.java

--- jeongyeon/member.java
+++ jeongyeon/member.java
@@ -3,26 +3,31 @@ package my_package;
 import java.io.DataOutputStream;
 
 public class member {
-	
-	private String name;
-	private String nameid;
-	private DataOutputStream writer;
-	
+
+	private String name; // 닉네임
+	private long nameid; // 회원 고유번호
+	private DataOutputStream writer; // 송출부
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getNameid() {
+
+	public long getNameid() {
 		return nameid;
 	}
-	public void setNameid(String nameid) {
+
+	public void setNameid(long nameid) {
 		this.nameid = nameid;
 	}
+
 	public DataOutputStream getWriter() {
 		return writer;
 	}
+
 	public void setWriter(DataOutputStream writer) {
 		this.writer = writer;
 	}
